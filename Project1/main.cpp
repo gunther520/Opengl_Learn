33b480ef5fd9d783b6ef5e77e2e6d9065e2f5bf3
Hoi Kan NG <hkngae@connect.ust.hk>
2024-10-04 17:27:50 +0800

    test

--- Project1/main.cpp
+++ Project1/main.cpp
@@ -1,8 +1,12 @@
+
 #include <glad/glad.h>
 #include <GLFW/glfw3.h>
 #include <iostream>
 #include <random>
 #include "shader.h"
+#define STB_IMAGE_IMPLEMENTATION
+#include "stb_image/stb_image.h"
+
 
 
 void framebuffer_size_callback(GLFWwindow* window, int width, int height)
@@ -68,16 +72,34 @@ int main() {
     };
 
     float vertices2[] = {
-        // positions         // colors
-         0.5f, -0.5f, 0.0f, 1.0f, 0.0f, 0.0f, // bottom right
-        -0.5f, -0.5f, 0.0f, 0.0f, 1.0f, 0.0f, // bottom left
-         0.0f,  0.5f, 0.0f, 0.0f, 0.0f, 1.0f  // top
+        // positions        // colors         // texture coords
+         0.5f,  0.5f, 0.0f, 1.0f, 0.0f, 0.0f, 1.0f, 1.0f, // top right
+         0.5f, -0.5f, 0.0f, 0.0f, 1.0f, 0.0f, 1.0f, 0.0f, // bottom right
+        -0.5f, -0.5f, 0.0f, 0.0f, 0.0f, 1.0f, 0.0f, 0.0f, // bottom left
+        -0.5f,  0.5f, 0.0f, 1.0f, 1.0f, 0.0f, 0.0f, 1.0f  // top left
     };
 
+    float texCoords[] = {
+            0.0f, 0.0f, // lower-left corner
+            1.0f, 0.0f, // lower-right corner
+            0.5f, 1.0f  // top-center corner
+    };
+
+    unsigned int indices[] = {
+		0, 1, 3, // first triangle
+		1, 2, 3  // second triangle
+	};
+
 
 // !!!! The sequence of the objects is important!!!!
 // !!!! The sequence of the objects is important!!!!
 // !!!! The sequence of the objects is important!!!!
+
+    unsigned int EBO;
+    glGenBuffers(1, &EBO);
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+    glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
+
 
     unsigned int VAO[2]; // Usage: determine how to retrieve the vertex data
     glGenVertexArrays(2, VAO);
@@ -98,12 +120,15 @@ int main() {
     glBindBuffer(GL_ARRAY_BUFFER, VBO[1]);
 
     glBufferData(GL_ARRAY_BUFFER, sizeof(vertices2), vertices2, GL_STATIC_DRAW);
-    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float),
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(float),
         (void*)0);
     glEnableVertexAttribArray(0);
-    glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float),
+    glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(float),
 		(void*)(3 * sizeof(float)));
     glEnableVertexAttribArray(1);
+    glVertexAttribPointer(2, 2, GL_FLOAT, GL_FALSE, 8 * sizeof(float),
+        (void*)(6 * sizeof(float)));
+    glEnableVertexAttribArray(2);
     glBindVertexArray(0);
 
 
@@ -112,6 +137,32 @@ int main() {
 
     Shader ourShader1("res/vertexShader1.txt", "res/fragmentShader1.txt");
     Shader ourShader2("res/vertexShader2.txt", "res/fragmentShader2.txt");
+
+
+
+
+    unsigned int texture;
+    glGenTextures(1, &texture);
+    glBindTexture(GL_TEXTURE_2D, texture);
+    // set the texture wrapping/filtering options (on currently bound texture)
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+    // load and generate the texture
+    int width, height, nrChannels;
+    unsigned char* data = stbi_load("res/container.jpg", &width, &height,
+        &nrChannels, 0);
+    if (data) {
+        glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, width, height, 0, GL_RGB,
+            GL_UNSIGNED_BYTE, data);
+        glGenerateMipmap(GL_TEXTURE_2D);
+    }
+    else
+    {
+        std::cout << "Failed to load texture" << std::endl;
+    }
+    stbi_image_free(data);
 
 
     // Tell OpenGL how to interpret the vertex data in this case 3 floats per positional vertex
@@ -147,13 +198,16 @@ int main() {
         glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
         glClear(GL_COLOR_BUFFER_BIT);
 
+
+        glBindTexture(GL_TEXTURE_2D, texture);
+        
         glBindVertexArray(VAO[1]);
-        ourShader2.use();
-        ourShader2.setFloat("offset", -0.3f);
-        glDrawArrays(GL_TRIANGLES, 0, 3);
+      
+        
+        glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
 
+        glBindTexture(GL_TEXTURE_2D, 0);
         glBindVertexArray(0);
-
         glBindVertexArray(VAO[0]);
         ourShader1.use();
         glDrawArrays(GL_TRIANGLES, 0, 3);
